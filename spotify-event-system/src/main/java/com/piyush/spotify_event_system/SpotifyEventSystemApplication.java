b9a84b12f84932289c1ec7c5666531ef6c9ea1a1
Piyush <157813396+PiyushJ1@users.noreply.github.com>
2025-12-26 02:35:51 +1100

    Add files via upload

---
+++ spotify-event-system/src/main/java/com/piyush/spotify_event_system/SpotifyEventSystemApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.spotify_event_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyEventSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyEventSystemApplication.class, args);
+	}
+
+}
