6611e9ce5a6b8ecdb71c2fe2fbcf3f5b391e9aba
PiyushJ1 <piyxsh@gmail.com>
2025-12-27 00:24:44 +1100

    feat: create data access layer to save to db

--- src/main/java/com/piyush/spotify_event_system/repo/ListeningEventRepo.java
+++ src/main/java/com/piyush/spotify_event_system/repo/ListeningEventRepo.java
@@ -1,4 +1,10 @@
 package com.piyush.spotify_event_system.repo;
 
-public interface ListeningEventRepo {
+import com.piyush.spotify_event_system.model.ListeningEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ListeningEventRepo extends JpaRepository<ListeningEvent, UUID> {
+    // db functions are handled automatically
 }
