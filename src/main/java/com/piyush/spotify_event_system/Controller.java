b2276fbe176ce214b338dd914b3403426e8b8ba3
PiyushJ1 <piyxsh@gmail.com>
2025-12-27 00:06:46 +1100

    refactor: remove testing profile class

--- src/main/java/com/piyush/spotify_event_system/Controller.java
+++ src/main/java/com/piyush/spotify_event_system/Controller.java
@@ -1,6 +1,5 @@
 package com.piyush.spotify_event_system;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,34 +10,8 @@ public class Controller {
     }
 
     @PostMapping("/api/setProfile")
-    public void receive(@RequestBody Profile profile) {
-        System.out.println("your name is: " + profile.getName());
-        System.out.println("you live at: " + profile.getAddress());
+    public void receive(@RequestBody String name) {
+        System.out.println("welcome: " + name);
     }
 }
 
-class Profile {
-
-    private String name;
-    private String address;
-
-    public Profile() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-
-}
