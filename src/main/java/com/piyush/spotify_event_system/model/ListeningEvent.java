304bc1d9c8c8df3d9ee640fbd85699f96aa9f136
PiyushJ1 <piyxsh@gmail.com>
2025-12-27 00:07:14 +1100

    feat: create listening even model for database

--- src/main/java/com/piyush/spotify_event_system/model/ListeningEvent.java
+++ src/main/java/com/piyush/spotify_event_system/model/ListeningEvent.java
@@ -1,4 +1,44 @@
 package com.piyush.spotify_event_system.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "listening_events")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ListeningEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "track_id", nullable = false)
+    private String trackId;
+
+    private String trackName;
+    private String artistName;
+    private String albumName;
+
+    @Column(nullable = false)
+    private LocalDateTime playedAt;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
+
