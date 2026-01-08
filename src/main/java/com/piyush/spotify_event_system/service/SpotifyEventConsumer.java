494a8b84faa5db8d10e3e5943bc290a974541d46
PiyushJ1 <piyxsh@gmail.com>
2026-01-08 17:40:09 +1100

    feat: create basic event consumption

--- src/main/java/com/piyush/spotify_event_system/service/SpotifyEventConsumer.java
+++ src/main/java/com/piyush/spotify_event_system/service/SpotifyEventConsumer.java
@@ -1,4 +1,31 @@
 package com.piyush.spotify_event_system.service;
 
+import com.piyush.spotify_event_system.dto.SpotifyEvent;
+import com.piyush.spotify_event_system.model.ListeningEvent;
+import com.piyush.spotify_event_system.repo.ListeningEventRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SpotifyEventConsumer {
+    private final ListeningEventRepo repo;
+
+    @KafkaListener(topics = "spotify-listening-events", groupId = "spotify-analytics-group")
+    public void consume(SpotifyEvent event) {
+        System.out.println("event received from Kafka: " + event.trackName());
+
+        ListeningEvent entity = ListeningEvent.builder()
+                .userId(event.userId())
+                .trackId(event.trackId())
+                .trackName(event.trackName())
+                .artistName(event.artistName())
+                .albumName(event.albumName())
+                .playedAt(event.playedAt())
+                .build();
+
+        repo.save(entity);
+        System.out.println("saved event to db: " + entity.getId());
+    }
 }
