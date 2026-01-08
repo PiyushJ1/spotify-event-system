d2b6d6adc6cd0b6ed9901339770966b14f729ad9
PiyushJ1 <piyxsh@gmail.com>
2026-01-08 17:40:25 +1100

    feat: create basic event production

--- src/main/java/com/piyush/spotify_event_system/service/SpotifyEventProducer.java
+++ src/main/java/com/piyush/spotify_event_system/service/SpotifyEventProducer.java
@@ -1,4 +1,18 @@
 package com.piyush.spotify_event_system.service;
 
+import com.piyush.spotify_event_system.dto.SpotifyEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SpotifyEventProducer {
+    private final KafkaTemplate<String, SpotifyEvent> kafkaTemplate;
+    private static final String TOPIC = "spotify-listening-events";
+
+    public void sendEvent(SpotifyEvent event) {
+        kafkaTemplate.send(TOPIC, event.userId().toString(), event);
+        System.out.println("sent event to Kafka: " + event.trackName());
+    }
 }
