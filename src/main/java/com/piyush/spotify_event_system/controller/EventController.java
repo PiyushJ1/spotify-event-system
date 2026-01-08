e0c19d187481e1aec4f993a6f4b4d822515df9fd
PiyushJ1 <piyxsh@gmail.com>
2026-01-08 17:41:14 +1100

    feat: create event controller with post endpoint

--- src/main/java/com/piyush/spotify_event_system/controller/EventController.java
+++ src/main/java/com/piyush/spotify_event_system/controller/EventController.java
@@ -1,17 +1,29 @@
-package com.piyush.spotify_event_system;
+package com.piyush.spotify_event_system.controller;
 
+import com.piyush.spotify_event_system.dto.SpotifyEvent;
+import com.piyush.spotify_event_system.service.SpotifyEventProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class Controller {
-    @GetMapping("/api/test/{id}")
-    public String getString(@PathVariable String id) {
-        return "hello !" + id;
-    }
+@RequiredArgsConstructor
+public class EventController {
+//    @GetMapping("/api/test/{id}")
+//    public String getString(@PathVariable String id) {
+//        return "hello !" + id;
+//    }
+//
+//    @PostMapping("/api/setProfile")
+//    public void receive(@RequestBody String name) {
+//        System.out.println("welcome: " + name);
+//    }
 
-    @PostMapping("/api/setProfile")
-    public void receive(@RequestBody String name) {
-        System.out.println("welcome: " + name);
+    private final SpotifyEventProducer producer;
+    @PostMapping("/api/event")
+    public ResponseEntity<String> simulateEvent(@RequestBody SpotifyEvent event) {
+        producer.sendEvent(event);
+        return ResponseEntity.accepted().body("event queued for processing");
     }
 }
 
